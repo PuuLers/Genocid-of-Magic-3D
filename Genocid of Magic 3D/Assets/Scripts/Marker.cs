d43113063eab55d94613044eb6f9bf6f6e3b521c
cans1z <canss1zzz@gmail.com>
2023-08-22 17:40:19 +0300

    f

--- Genocid of Magic 3D/Assets/Scripts/Marker.cs
+++ Genocid of Magic 3D/Assets/Scripts/Marker.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Maker : MonoBehaviour
+public class Marker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float rotationSpeed = 10f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+        currentRotation.y += rotationSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(currentRotation);
     }
 }
